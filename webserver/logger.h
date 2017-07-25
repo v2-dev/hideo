c0f7a0d4c1bebcc06bb3832d9e025c6157b94337
v2-dev <viskyria@gmail.com>
2017-07-25 16:00:30 +0200

    head changes

--- webserver/logger.h
+++ webserver/logger.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h> 
+#include <errno.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -10,13 +10,13 @@
 #include <pthread.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <sys/syscall.h> 
+#include <sys/syscall.h>
 
 #define _GNU_SOURCE
 
 #define SIZE_TO_FFLUSH 30
 
-#define ERR	0x01		
+#define ERR	0x01
 #define	WRN	0x02
 #define NFO	0x03
 
@@ -26,8 +26,11 @@ struct logger {
 
 	FILE *f;
 	int loglvl;
-	
+
 };
+
+pthread_mutex_t log_mutex;
+pthread_cond_t log_cond;
 
 struct logger * srvlog;
 
