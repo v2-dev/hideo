e34dde41887140e265967492a748871b98bb1bdf
v2-dev <viskyria@gmail.com>
2017-07-25 17:02:18 +0200

    added thread_logger_manager

--- webserver/logger.c
+++ webserver/logger.c
@@ -22,30 +22,127 @@ struct logger * create_logger(char * logPath, int loglvl){	/* crea un nuovo logg
 	return myLogger;
 }
 
+void *thread_logger_manager()
+{
+	int err = 0;
+
+	for(;;){
+
+		if ((err = pthread_mutex_lock(&logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_mutex_lock: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+		while ((strlen(MSG_LOG) < 1) {
+			if ((err = pthread_cond_wait(&logger_cond, &pool_mutex)) != 0) {
+				fprintf(stderr, "Error in pthread_cond_wait: %d : %s\n", err, strerror(err));
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		fprintf(srvlog->f, MSG_LOG);
+
+		if ((err = pthread_mutex_unlock(&logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_mutex_unlock: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+
+	}
+}
+
 
 void toLog(int msgt, char * msg, struct logger * myLogger){
 
 	pid_t tid;
+	int err;
 	tid = syscall(SYS_gettid);
+
+	if ((err = pthread_mutex_lock(&logger_mutex)) != 0) {
+		fprintf(stderr, "Error in pthread_mutex_lock: %d : %s\n", err, strerror(err));
+		exit(EXIT_FAILURE);
+	}
+
+	memset(MSG_LOG, 0, MSG_SIZE);
+
+	if ((err = pthread_mutex_unlock(&logger_mutex)) != 0) {
+		fprintf(stderr, "Error in pthread_mutex_unlock: %d : %s\n", err, strerror(err));
+		exit(EXIT_FAILURE);
+	}
 
 	if ((msgt==ERR)&&((ERR) & (myLogger->loglvl))){
 
 		//fprintf(stderr, "[ERR] [%d] %s\n", tid, msg); //da togliere se non voglio stampare il msg sullo schermo
-		fprintf(myLogger->f, "[ERR] [%d] %s\n", tid, msg);
+
+		//WAKE UP! signal for thread_logger_manager
+		if ((err = pthread_mutex_lock(&logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_mutex_lock: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+		sprintf(MSG_LOG, "[ERR] [%d] %s\n", tid, msg);
+
+		if ((err = pthread_cond_signal(&logger_cond, &logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_cond_signal: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+		if ((err = pthread_mutex_unlock(&logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_mutex_unlock: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
 	}
 
 
 	else if ((msgt==WRN)&&((WRN) & (myLogger->loglvl))){
 
 		//fprintf(stdout, "[WRN] [%d] %s\n", tid, msg);  //da togliere se non voglio stampare il msg sullo schermo
-		fprintf(myLogger->f, "[WRN] [%d] %s\n", tid, msg);
+		if ((err = pthread_mutex_lock(&logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_mutex_lock: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+		sprintf(MSG_LOG, "[WRN] [%d] %s\n", tid, msg);
+
+		//WAKE UP! signal for thread_logger_manager
+		if ((err = pthread_cond_signal(&logger_cond, &logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_cond_signal: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+		if ((err = pthread_mutex_unlock(&logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_mutex_unlock: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+		//fprintf(myLogger->f, "[WRN] [%d] %s\n", tid, msg);
 
 	}
 
 	else if ((msgt==NFO)&&((NFO) & ((myLogger->loglvl)>>2))){
 
 		//fprintf(stdout, "[NFO] [%d] %s\n", tid, msg);  //da togliere se non voglio stampare il msg sullo schermo
-		fprintf(myLogger->f, "[NFO] [%d] %s\n", tid, msg);
+		if ((err = pthread_mutex_lock(&logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_mutex_lock: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+		sprintf(MSG_LOG, "[INFO] [%d] %s\n", tid, msg);
+
+		//WAKE UP! signal for thread_logger_manager
+		if ((err = pthread_cond_signal(&logger_cond, &logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_cond_signal: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+		if ((err = pthread_mutex_unlock(&logger_mutex)) != 0) {
+			fprintf(stderr, "Error in pthread_mutex_unlock: %d : %s\n", err, strerror(err));
+			exit(EXIT_FAILURE);
+		}
+
+		//fprintf(myLogger->f, "[NFO] [%d] %s\n", tid, msg);
 	}
+
 
 }
--- webserver/logger.h
+++ webserver/logger.h
@@ -15,6 +15,7 @@
 #define _GNU_SOURCE
 
 #define SIZE_TO_FFLUSH 30
+#define MSG_SIZE	4096
 
 #define ERR	0x01
 #define	WRN	0x02
@@ -29,12 +30,17 @@ struct logger {
 
 };
 
-pthread_mutex_t log_mutex;
-pthread_cond_t log_cond;
-
 struct logger * srvlog;
+char MSG_LOG[MSG_SIZE];
 
+pthread_mutex_t logger_mutex;
+pthread_cond_t logger_cond;
+
+pthread_mutex_t logger_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t logger_cond = PTHREAD_COND_INITIALIZER;
 
 struct logger * alloc_logger(void);
 struct logger * create_logger(char *, int);	/* PROTOTIPI DELLE FUNZIONI */
+
 void toLog(int msgt, char * msg, struct logger * myLogger);
+void *thread_logger_manager();
